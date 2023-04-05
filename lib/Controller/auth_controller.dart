e6d9773ff8a2310b260dd1835ccbb79046ccdcf1
ADIAWIAKOYE <adiawiakoye.le10@gmail.com>
2023-04-05 17:50:41 +0000

    fin de add profile

--- lib/Controller/auth_controller.dart
+++ lib/Controller/auth_controller.dart
@@ -1,26 +1,27 @@
 import 'dart:ffi';
+import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-
 import '../Views/Profile/add_profile.dart';
 import '../Views/home/home_screen.dart';
-
+import 'package:path/path.dart' as Path;
 
 class AuthController extends GetxController {
-
   FirebaseAuth auth = FirebaseAuth.instance;
 
   var isLoading = false.obs;
 
-  void login(BuildContext context,{String? email, String? password}) {
-
+  void login(BuildContext context, {String? email, String? password}) {
     isLoading(true);
 
-    auth.signInWithEmailAndPassword(email: email!, password: password!)
+    auth
+        .signInWithEmailAndPassword(email: email!, password: password!)
         .then((value) {
       /// Login Success
       // Get.to(()=> HomeScreen());
@@ -41,15 +42,16 @@ class AuthController extends GetxController {
     });
   }
 
-  void signUp(BuildContext context,{String? email, String? password}){
+  void signUp(BuildContext context, {String? email, String? password}) {
     ///ici, nous devons fournir deux choses
     ///1- email
     ///2- password
-    
-   isLoading(true);
 
-    auth.createUserWithEmailAndPassword(email: email!, password: password!)
-    .then((value){
+    isLoading(true);
+
+    auth
+        .createUserWithEmailAndPassword(email: email!, password: password!)
+        .then((value) {
       isLoading(false);
 
       /// Naviguer l'utilisateur vers l'écran de profil
@@ -59,12 +61,11 @@ class AuthController extends GetxController {
         context,
         MaterialPageRoute(builder: (context) => ProfileScreen()),
       );
-
-    }).catchError((e){
+    }).catchError((e) {
       /// afficher les informations d'erreur
-      
+
       print("Erreur d'authentification $e");
-      
+
       isLoading(false);
     });
   }
@@ -73,11 +74,13 @@ class AuthController extends GetxController {
     auth.sendPasswordResetEmail(email: email).then((value) {
       Navigator.pop(context);
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Nous avons envoyé un e-mail de réinitialisation du mot de passe'),
+        content: Text(
+            'Nous avons envoyé un e-mail de réinitialisation du mot de passe'),
         duration: Duration(seconds: 10),
       ));
     }).catchError((e) {
-      print("Erreur d'envoi de l'e-mail de réinitialisation du mot de passe est $e");
+      print(
+          "Erreur d'envoi de l'e-mail de réinitialisation du mot de passe est $e");
     });
   }
 
@@ -87,7 +90,8 @@ class AuthController extends GetxController {
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
     // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
 
     // Create a new credential
     final credential = GoogleAuthProvider.credential(
@@ -106,10 +110,55 @@ class AuthController extends GetxController {
       isLoading(false);
       print("Error is $e");
     });
-
   }
 
   var isProfileInformationLoading = false.obs;
 
+  Future<String> uploadImageToFirebaseStorage(File image) async {
+    String imageUrl = '';
+    String fileName = Path.basename(image.path);
 
+    var reference =
+        FirebaseStorage.instance.ref().child('profileImages/$fileName');
+    UploadTask uploadTask = reference.putFile(image);
+    TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
+    await taskSnapshot.ref.getDownloadURL().then((value) {
+      imageUrl = value;
+    }).catchError((e) {
+      print("Error happen $e");
+    });
+
+    return imageUrl;
+  }
+
+  void uploadProfileData(
+      BuildContext context,
+      String imageUrl,
+      String firstName,
+      String lastName,
+      String mobileNumber,
+      String dob,
+      String gender) {
+    String uid = FirebaseAuth.instance.currentUser!.uid;
+
+    print(
+        "lid est est --------------------------------------------------------- " +
+            uid);
+    FirebaseFirestore.instance.collection('users').doc(uid).set({
+      'image': imageUrl,
+      'first': firstName,
+      'last': lastName,
+      'dob': dob,
+      'gender': gender
+    }).then((value) {
+      isProfileInformationLoading(false);
+      //Get.offAll(() => HomeScreen());
+
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+        (route) => false,
+      );
+    });
+  }
 }
--- lib/Views/Profile/add_profile.dart
+++ lib/Views/Profile/add_profile.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -11,10 +9,10 @@ import '../../Utils/app_color.dart';
 import '../../Widgets/my_widgets.dart';
 
 class ProfileScreen extends StatefulWidget {
-
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
 }
+
 class _ProfileScreenState extends State<ProfileScreen> {
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
@@ -30,6 +28,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
       dob.text = '${picked.day}-${picked.month}-${picked.year}';
     }
   }
+
   TextEditingController firstNameController = TextEditingController();
   TextEditingController lastNameController = TextEditingController();
   TextEditingController mobileNumberController = TextEditingController();
@@ -48,13 +47,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 onTap: () async {
                   final ImagePicker _picker = ImagePicker();
                   final XFile? image =
-                  await _picker.pickImage(source: ImageSource.camera);
+                      await _picker.pickImage(source: ImageSource.camera);
                   if (image != null) {
                     profileImage = File(image.path);
                     setState(() {});
                     Navigator.pop(context);
                   }
-
                 },
                 child: Icon(
                   Icons.camera_alt,
@@ -75,7 +73,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     setState(() {});
                     Navigator.pop(context);
                   }
-
                 },
                 child: Image.asset(
                   'assets/gallary.png',
@@ -106,14 +103,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-   // authController = Get.find<AuthController>();
-
+    authController = Get.find<AuthController>();
   }
-
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Container(
         margin: EdgeInsets.symmetric(horizontal: Get.width * 0.05),
@@ -156,21 +150,21 @@ class _ProfileScreenState extends State<ProfileScreen> {
                           ),
                           child: profileImage == null
                               ? CircleAvatar(
-                            radius: 56,
-                            backgroundColor: Colors.white,
-                            child: Icon(
-                              Icons.camera_alt,
-                              color: Colors.orange,
-                              size: 50,
-                            ),
-                          )
+                                  radius: 56,
+                                  backgroundColor: Colors.white,
+                                  child: Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.black54,
+                                    size: 50,
+                                  ),
+                                )
                               : CircleAvatar(
-                            radius: 56,
-                            backgroundColor: Colors.white,
-                            backgroundImage: FileImage(
-                              profileImage!,
-                            ),
-                          ),
+                                  radius: 56,
+                                  backgroundColor: Colors.white,
+                                  backgroundImage: FileImage(
+                                    profileImage!,
+                                  ),
+                                ),
                         ),
                       ],
                     ),
@@ -186,7 +180,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       if (firstNameController.text.isEmpty) {
                         Get.snackbar('Warning', 'First Name is required.',
                             colorText: Colors.white,
-                            backgroundColor: Colors.blue );
+                            backgroundColor: Colors.blue);
                         return '';
                       }
                     }),
@@ -236,7 +230,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         'assets/calender.png',
                         cacheHeight: 20,
                       ),
-                      hintText: 'Date de naissance',
+                      hintText: 'date de naissance',
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8.0),
                       ),
@@ -247,29 +241,29 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   children: [
                     Expanded(
                         child: Container(
-                          // alignment: Alignment.topLeft,
-                          // width: 150,
-                          child: RadioListTile(
-                            title: Text(
-                              'Homme',
-                              style: TextStyle(
-                                fontSize: 19,
-                                fontWeight: FontWeight.w400,
-                                color: AppColors.genderTextColor,
-                              ),
-                            ),
-                            value: 0,
-                            groupValue: selectedRadio,
-                            onChanged: (int? val) {
-                              setSelectedRadio(val!);
-                            },
+                      // alignment: Alignment.topLeft,
+                      // width: 150,
+                      child: RadioListTile(
+                        title: Text(
+                          'Male',
+                          style: TextStyle(
+                            fontSize: 19,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.genderTextColor,
                           ),
-                        )),
+                        ),
+                        value: 0,
+                        groupValue: selectedRadio,
+                        onChanged: (int? val) {
+                          setSelectedRadio(val!);
+                        },
+                      ),
+                    )),
                     Expanded(
                       child: Container(
                         child: RadioListTile(
                           title: Text(
-                            'Femme',
+                            'Female',
                             style: TextStyle(
                               fontSize: 19,
                               fontWeight: FontWeight.w400,
@@ -286,45 +280,53 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     ),
                   ],
                 ),
-              //  Obx(()=> authController!.isProfileInformationLoading.value? Center(child: CircularProgressIndicator(),) :
-                Container(
-                  height: 50,
-                  margin: EdgeInsets.only(top: Get.height * 0.02),
-                  width: Get.width,
-                  child: elevatedButton(
-                    text: 'Envoyer',
-                    onpress: () async{
-                      if (dob.text.isEmpty) {
-                        Get.snackbar(
-                            'Warning', "Date of birth is required.",
-                            colorText: Colors.white,
-                            backgroundColor: Colors.blue);
-                        return '';
-                      }
+                Obx(
+                  () => authController!.isProfileInformationLoading.value
+                      ? Center(
+                          child: CircularProgressIndicator(),
+                        )
+                      : Container(
+                          height: 50,
+                          margin: EdgeInsets.only(top: Get.height * 0.02),
+                          width: Get.width,
+                          child: elevatedButton(
+                            text: 'Envoyer',
+                            onpress: () async {
+                              if (dob.text.isEmpty) {
+                                Get.snackbar(
+                                    'Warning', "Date of birth is required.",
+                                    colorText: Colors.white,
+                                    backgroundColor: Colors.blue);
+                                return '';
+                              }
 
-                      if (!formKey.currentState!.validate()) {
-                        return null;
-                      }
+                              if (!formKey.currentState!.validate()) {
+                                return null;
+                              }
 
-                      if(profileImage == null){
-                        Get.snackbar(
-                            'Warning', "Image is required.",
-                            colorText: Colors.white,
-                            backgroundColor: Colors.blue);
-                        return null;
-                      }
+                              if (profileImage == null) {
+                                Get.snackbar('Warning', "Image is required.",
+                                    colorText: Colors.white,
+                                    backgroundColor: Colors.blue);
+                                return null;
+                              }
+                              authController!.isProfileInformationLoading(true);
 
-                    },
-                  ),
-                 ),
-                //),
-                  //    authController!.isProfileInformationLoading(true);
+                              String imageUrl = await authController!
+                                  .uploadImageToFirebaseStorage(profileImage!);
 
-                   //   String imageUrl = await authController!.uploadImageToFirebaseStorage(profileImage!);
-
-                   //   authController!.uploadProfileData(imageUrl, firstNameController.text.trim(), lastNameController.text.trim(), mobileNumberController.text.trim(), dob.text.trim(), selectedRadio ==0 ? "Male": "Female");
-
-
+                              authController!.uploadProfileData(
+                                  context,
+                                  imageUrl,
+                                  firstNameController.text.trim(),
+                                  lastNameController.text.trim(),
+                                  mobileNumberController.text.trim(),
+                                  dob.text.trim(),
+                                  selectedRadio == 0 ? "Male" : "Female");
+                            },
+                          ),
+                        ),
+                ),
                 SizedBox(
                   height: Get.height * 0.03,
                 ),
@@ -338,7 +340,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
                             style: TextStyle(
                                 color: Color(0xff262628), fontSize: 13)),
                         TextSpan(
-                            text: 'termes, politique de données et politique de cookies',
+                            text:
+                                'termes, politique de données et politique de cookies',
                             style: TextStyle(
                                 color: Color(0xff262628),
                                 fontSize: 13,
@@ -352,6 +355,4 @@ class _ProfileScreenState extends State<ProfileScreen> {
       ),
     );
   }
-
-
 }
